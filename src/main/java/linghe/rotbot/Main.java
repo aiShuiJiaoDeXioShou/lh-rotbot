62cea44c0b02e6ca48d9c9eac2042fcdbfebf8da
aiShuiJiaoDeXioShou <2832294398@qq.com>
2023-08-19 11:33:50 +0800

    更新~

--- src/main/java/linghe/rotbot/Main.java
+++ src/main/java/linghe/rotbot/Main.java
@@ -1,5 +1,9 @@
 package linghe.rotbot;
 
+import com.github.kwhat.jnativehook.GlobalScreen;
+import com.github.kwhat.jnativehook.NativeHookException;
+import linghe.rotbot.listener.GlobalKeyListener;
+import linghe.rotbot.listener.GlobalListener;
 import linghe.rotbot.service.RobotService;
 
 import java.net.URL;
@@ -7,13 +11,28 @@ import java.net.URL;
 public class Main {
     public static void main(String[] args) throws Exception {
 //        init();
-        RobotService robotService = new RobotService();
-        robotService.startWindowRecord();
+        listener();
     }
 
     public static void init() {
         // 初始化OpenCv
         URL url = ClassLoader.getSystemResource("lib/opencv_java480.dll");
         System.load(url.getPath());
+    }
+
+    // 注册全局监听事件
+    public static void listener() {
+        try {
+            GlobalScreen.registerNativeHook();
+        } catch (NativeHookException ex) {
+            System.err.println("There was a problem registering the native hook.");
+            System.err.println(ex.getMessage());
+            System.exit(1);
+        }
+        GlobalListener globalListener = new GlobalListener();
+        GlobalScreen.addNativeKeyListener(globalListener);
+        GlobalScreen.addNativeMouseListener(globalListener);
+        GlobalScreen.addNativeMouseWheelListener(globalListener);
+        GlobalScreen.addNativeMouseMotionListener(globalListener);
     }
 }
